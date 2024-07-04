82e1bc42ed8f9bee5d5072696b6859a4ea05ecea
Chris Chiu <chris820614@gmail.com>
2024-07-05 02:39:02 +0800

    update_news_func

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+import requests
 import os
 
 app = Flask(__name__)
@@ -9,6 +10,11 @@ app = Flask(__name__)
 # Line API 初始化
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+news_api_key = os.getenv('NEWS_API_KEY')
+
+@app.route("/", methods=['GET'])
+def index():
+    return "Hello, this is the Line bot application."
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -24,8 +30,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    reply = f"你說了: {msg}"
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+    if msg == 'now':
+        news = fetch_news()
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=news)) 
+    else:
+        reply = f"你說了: {msg}"
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
 
 @handler.add(PostbackEvent)
 def handle_postback(event):
@@ -39,6 +49,18 @@ def welcome(event):
     name = profile.display_name
     message = TextSendMessage(text=f'{name} 歡迎加入')
     line_bot_api.reply_message(event.reply_token, message)
+
+def fetch_news():
+    url = f'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={news_api_key}'
+    responses = request.url(url)
+    if requests.status_codes == 200:
+        news_data = responses.json()
+        articles = news_data['articles'][:5]
+        news_list = [f"{article['title']}: {article['url']}" for article in articles]
+        return '\n'.join(news_list)
+    else:
+        return "無法獲取新聞"
+        
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
